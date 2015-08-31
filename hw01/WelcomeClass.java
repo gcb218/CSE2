0c137c1d958d69db7e1a4399e0f4fba3ef02106a
gcb218 <gcb218@lehigh.edu>
2015-08-31 23:28:51 +0000

    First commit

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,28 @@
+///////////////////////////////////////////////////////////////////////////////////
+//Brian Chen
+//Hello World Java Program
+//
+//      first homework assignment
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass//
+
+//  define a class
+public class WelcomeClass{
+    
+//  add main method
+public static void main(String[] args) {
+    
+//  print the statement
+    System.out.println(" ----------- ");
+    System.out.println(" | WELCOME | ");
+    System.out.println(" ----------- ");
+    System.out.println("^ ^ ^ ^ ^ ^ ^ ^ ^");
+     System.out.println("/\\/\\/\\/\\/\\/\\/\\/\\/\\");
+    System.out.println("<-G--C--B--2--1--8->");
+     System.out.println("\\/\\/\\/\\/\\/\\/\\/\\/\\/");
+    System.out.println(" v v v v v v v v v");
+    
+    System.out.println("I am from St. Louis, MO and was born in Atlanta, GA. This summer I had an internship with a large healthcare corporation.");
+}
+}
